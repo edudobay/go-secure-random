0ab90c2ad9b6c8843b7232342dc73d82f9f8c6f2
Eduardo Dobay <edudobay@gmail.com>
2019-11-15 12:52:01 -0300

    Use goroutines to print without great benchmark disturbances

--- main_secrets/main.go
+++ main_secrets/main.go
@@ -9,6 +9,24 @@ import (
 )
 
 const DefaultBytes = 32
+const PrintQueueSize = 4096
+
+func printTokens(tokens chan string, printer func(string), quit chan bool) {
+	for token := range tokens {
+		printer(token)
+	}
+	quit <- true
+}
+
+func printer(quiet bool) func(string) {
+	if !quiet {
+		return func(token string) {
+			fmt.Println(token)
+		}
+	} else {
+		return func(_ string) {}
+	}
+}
 
 func main() {
 	var bytes = flag.Int("bytes", DefaultBytes, "byte length of generated secret")
@@ -17,14 +35,19 @@ func main() {
 
 	flag.Parse()
 
+	tokens := make(chan string, PrintQueueSize)
+	quit := make(chan bool)
+
 	loop := func() {
-		token := secrets.GenToken(*bytes)
-		if !*quiet {
-			fmt.Println(token)
-		}
+		tokens <- secrets.GenToken(*bytes)
 	}
 
+	go printTokens(tokens, printer(*quiet), quit)
+
 	result := benchmark.Run(loop, *count)
+
+	close(tokens)
+	<-quit
 
 	// Ignore errors
 	_, _ = fmt.Fprintf(os.Stderr, "Generated %d secrets in %v; average %.4f µs per secret",
